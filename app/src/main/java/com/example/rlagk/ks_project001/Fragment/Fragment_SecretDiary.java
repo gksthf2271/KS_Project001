7376025a88022cc477a517335642f0b1890c31ee
김한솔 <rlagksthf33@naver.com>
2018-04-11 23:26:07 +0900

    Firebase gradle 등록

--- app/src/main/java/com/example/rlagk/ks_project001/Fragment/Fragment_SecretDiary.java
+++ app/src/main/java/com/example/rlagk/ks_project001/Fragment/Fragment_SecretDiary.java
@@ -19,6 +19,6 @@ public class Fragment_SecretDiary extends android.support.v4.app.Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_SecretDiary, container, false);
+        return inflater.inflate(R.layout.fragment_secretdiary, container, false);
     }
 }
--- app/src/main/java/com/example/rlagk/ks_project001/Fragment/Fragment_ShareDiary.java
+++ app/src/main/java/com/example/rlagk/ks_project001/Fragment/Fragment_ShareDiary.java
@@ -19,6 +19,6 @@ public class Fragment_ShareDiary extends Fragment {
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ShareDiary, container, false);
+        return inflater.inflate(R.layout.fragment_sharediary, container, false);
     }
 }
