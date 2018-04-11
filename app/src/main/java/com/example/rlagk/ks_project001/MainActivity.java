6c1656e3f4292e9717feef12c253dba1d4b480bc
HSKim <gksthf2271@gmail.com>
2018-04-11 23:11:36 +0900

    Fragment guideline 생성

--- app/src/main/java/com/example/rlagk/ks_project001/MainActivity.java
+++ app/src/main/java/com/example/rlagk/ks_project001/MainActivity.java
@@ -1,15 +1,66 @@
 package com.example.rlagk.ks_project001;
 
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.rlagk.ks_project001.Fragment.Fragment_Main;
+import com.example.rlagk.ks_project001.Fragment.Fragment_ShareDiary;
+import com.example.rlagk.ks_project001.Fragment.Fragment_SecretDiary;
 
+public class MainActivity extends AppCompatActivity
+{
+    ViewPager vp;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("ahahah","ahahah");
+
+        vp = (ViewPager)findViewById(R.id.vp);
+
+        vp.setAdapter(new pagerAdapter(getSupportFragmentManager()));
+        vp.setCurrentItem(0);
+    }
+
+    View.OnClickListener movePageListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int tag = (int) v.getTag();
+            vp.setCurrentItem(tag);
+        }
+    };
+
+    private class pagerAdapter extends FragmentStatePagerAdapter
+    {
+        public pagerAdapter(android.support.v4.app.FragmentManager fm)
+        {
+            super(fm);
+        }
+        @Override
+        public android.support.v4.app.Fragment getItem(int position)
+        {
+            switch(position)
+            {
+                case 0:
+                    return new Fragment_Main();
+                case 1:
+                    return new Fragment_ShareDiary();
+                case 2:
+                    return new Fragment_SecretDiary();
+                default:
+                    return null;
+            }
+        }
+        @Override
+        public int getCount()
+        {
+            return 3;
+        }
     }
 }
+
