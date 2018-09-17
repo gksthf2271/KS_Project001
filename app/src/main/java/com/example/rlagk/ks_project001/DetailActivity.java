144e09463527116c3110c9f67405ee1a269f9bde
HSKim <gksthf2271@gmail.com>
2018-09-17 16:54:31 +0900

    DB setup 및 전반적인 UI 수정

--- app/src/main/java/com/example/rlagk/ks_project001/DetailActivity.java
+++ app/src/main/java/com/example/rlagk/ks_project001/DetailActivity.java
@@ -1,24 +1,38 @@
 package com.example.rlagk.ks_project001;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 
+import com.example.rlagk.ks_project001.DB.Contact;
+import com.example.rlagk.ks_project001.DB.DBHelperUtils;
+import com.example.rlagk.ks_project001.Fragment.Fragment_Main;
+import com.example.rlagk.ks_project001.View.DetailView;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
-/**
- * Created by xmuSistone on 2016/9/19.
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.IdentityHashMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class DetailActivity extends FragmentActivity {
     public static final String TAG = "DetailActivity";
 
@@ -26,21 +40,55 @@ public class DetailActivity extends FragmentActivity {
 
     public static final String IMAGE_TRANSITION_NAME = "transitionImage";
     public static final String ADDRESS1_TRANSITION_NAME = "address1";
-    public static final String ADDRESS2_TRANSITION_NAME = "address2";
-    public static final String ADDRESS3_TRANSITION_NAME = "address3";
-    public static final String ADDRESS4_TRANSITION_NAME = "address4";
-    public static final String ADDRESS5_TRANSITION_NAME = "address5";
     public static final String RATINGBAR_TRANSITION_NAME = "ratingBar";
+    public static long ID = 0;
 
-    public static final String HEAD1_TRANSITION_NAME = "head1";
-    public static final String HEAD2_TRANSITION_NAME = "head2";
-    public static final String HEAD3_TRANSITION_NAME = "head3";
-    public static final String HEAD4_TRANSITION_NAME = "head4";
+    @BindView(R.id.btnCancel)
+    Button mCancelBtn;
+    @BindView(R.id.btnSave)
+    Button mSaveBtn;
+    @BindView(R.id.cDetailView)
+    LinearLayout mDetailView;
+
+    private DBHelperUtils mDBHelperUtils;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         Log.d(TAG,"onCreate(...)");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        init();
+        ButterKnife.bind(this);
+    }
+
+    private void init(){
+        mDBHelperUtils = new DBHelperUtils(this);
+    }
+
+    @OnClick(R.id.btnCancel)
+    public void cancelBTNClick(){
+        Log.d(TAG,"cancelBTNClick(...)");
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.btnSave)
+    public void saveBTNClick(){
+        ID++;
+        Log.d(TAG,"saveBTNClick(...) " + ID);
+        long nowTime = System.currentTimeMillis();
+        Date date = new Date(nowTime);
+        EditText diaryText = mDetailView.findViewById(R.id.diaryText);
+        SimpleDateFormat idFormat = new SimpleDateFormat("yyyyMMdd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Contact contact = new Contact();
+        contact.setDate(simpleDateFormat.format(date));
+        contact.setTitle(diaryText.getText().toString());
+        contact.setDescription(diaryText.getText().toString());
+        contact.setId(String.valueOf(idFormat.format(date)) + ID);
+        mDBHelperUtils.addContact(contact);
+        diaryText.setText("");
     }
 }
--- app/src/main/java/com/example/rlagk/ks_project001/View/LockView.java
+++ app/src/main/java/com/example/rlagk/ks_project001/View/LockView.java
@@ -10,22 +10,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.rlagk.ks_project001.DetailActivity;
 import com.example.rlagk.ks_project001.R;
 
+import org.w3c.dom.Text;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LockView extends LinearLayout{
-    private static final String TAG = DetailView.class.getName();
+public class LockView extends RelativeLayout{
+    private static final String TAG = LockView.class.getName();
 
     @BindView(R.id.pwdView)
     LinearLayout mPwdView;
@@ -33,9 +37,8 @@ public class LockView extends LinearLayout{
     Button mLockBtn;
     @BindView(R.id.lockEditText)
     EditText mEditText;
-    @BindView(R.id.successView)
-    ImageView mSuccessView;
 
+    private btnClickCallbackListener mListener;
 
     public LockView(Context context) {
         this(context, null, 0);
@@ -87,13 +90,29 @@ public class LockView extends LinearLayout{
     }
 
     @OnClick(R.id.lockBtn)
-    void onButtonClicked(){
-        Log.d(TAG,"onButtonClicked(...)");
+    void onButtonClicked() {
+        Log.d(TAG, "onButtonClicked(...)");
         String dummyPwd = "123";
-        if(mEditText.getText().equals(dummyPwd)){
-            mPwdView.setVisibility(View.GONE);
-            mSuccessView.setVisibility(View.VISIBLE);
+        String inputData = mEditText.getText().toString();
+        if (inputData.equals(dummyPwd)) {
+            onBtnClickCallback();
+            InputMethodManager imm = (InputMethodManager) getContext().getSystemService(getContext().INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(mPwdView.getWindowToken(), 0);
+
         }
     }
 
+    public interface btnClickCallbackListener{
+        void onBtnClickCallback();
+    }
+
+    void onBtnClickCallback(){
+        if (mListener != null) {
+            mListener.onBtnClickCallback();
+        }
+    }
+
+    public void setBtnCallbacklistener(btnClickCallbackListener listener){
+        mListener = listener;
+    }
 }
